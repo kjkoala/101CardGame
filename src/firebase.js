53c4d1eb6974cb0cf64e451639b2e690a6bdeff5
IGOR <41189562+kjkoala@users.noreply.github.com>
2020-02-24 20:08:16 +0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
